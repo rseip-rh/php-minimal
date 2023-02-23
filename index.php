a0596ce4b70148baa128aaf583c8484175b2e822
Roger Seip <98971292+rseip-rh@users.noreply.github.com>
2023-02-22 20:38:02 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Minimal PHP Application</title>
+ </head>
+ <body>
+<?php echo "This minimal PHP application has been accessed at IP address " , $_SERVER['SERVER_ADDR']; ?> 
+ </body>
+</html>
